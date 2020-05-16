bbc23b6557d1f31c69c9843dd2885cd1e88034c5
VLAD-KIM <vlad.kim.1906@gmail.com>
2020-05-16 09:49:21 +0600

    Initial commit.

---
+++ TestTask/DoublyLinkedList.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTask
+{
+    public class DoublyLinkedList<T> : IEnumerable<T>
+    {
+        /// <summary>
+        /// Свойство для получения количества элементов в списке.
+        /// </summary>
+        public int Count { get; private set; }
+        Node<T> Head { get; set; }
+        Node<T> Tail { get; set; }
+
+        public DoublyLinkedList() { }
+
+        public DoublyLinkedList(T data)
+        {
+            Node<T> node = new Node<T>(data);
+            Head = node;
+            Tail = node;
+            Count = 1;
+        }
+
+        /// <summary>
+        /// Метод для вставки новых элементов в конец списка.
+        /// </summary>
+        /// <param name="data"></param>
+        public void Add(T data)
+        {
+            Node<T> node = new Node<T>(data);
+
+            if (Count == 0)
+            {
+                Head = node;
+                Tail = node;
+            }
+            else
+            {
+                Tail.Next = node;
+                node.Prev = Tail;
+                Tail = node;
+            }
+
+            Count++;
+        }
+
+        /// <summary>
+        /// Метод для вставки новых элементов в произвольную позицию, по индексу.
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="index"></param>
+        public void Insert(T data, int index)
+        {
+            Node<T> node = new Node<T>(data);
+
+            if (index == 0)
+            {
+                if (Count == 0)
+                {
+                    Add(data);
+                    return;
+                }
+                else
+                {
+                    Head.Prev = node;
+                    node.Next = Head;
+                    Head = node;
+                }
+            }
+            else if (index == Count)
+            {
+                Add(data);
+                return;
+            }
+            else if ((Count - index) > index)
+            {
+                Node<T> prev = Head;
+
+                for (int i = 0; i < index - 1; i++)
+                {
+                    prev = prev.Next;
+                }
+
+                node.Next = prev.Next;
+                node.Prev = prev;
+                prev.Next.Prev = node;
+                prev.Next = node;
+            }
+            else
+            {
+                Node<T> next = Tail;
+
+                for (int i = Count - 1; i > index; i--)
+                {
+                    next = next.Prev;
+                }
+
+                node.Next = next;
+                node.Prev = next.Prev;
+                next.Prev.Next = node;
+                next.Prev = node;
+            }
+
+            Count++;
+        }
+
+        /// <summary>
+        /// Метод для удаления элементов списка по индексу.
+        /// </summary>
+        /// <param name="index"></param>
+        public void RemoveAt(int index)
+        {
+            if (index == 0)
+            {
+                Head = Head.Next;
+                Head.Prev = null;
+            }
+            else if (index == Count - 1)
+            {
+                Tail = Tail.Prev;
+                Tail.Next = null;
+            }
+            else if ((Count - index) > index)
+            {
+                Node<T> prev = Head;
+
+                for (int i = 0; i < index - 1; i++)
+                {
+                    prev = prev.Next;
+                }
+
+                prev.Next = prev.Next.Next;
+                prev.Next.Prev = prev;
+            }
+            else
+            {
+                Node<T> next = Tail;
+
+                for (int i = Count - 1; i > index + 1; i--)
+                {
+                    next = next.Prev;
+                }
+
+                next.Prev = next.Prev.Prev;
+                next.Prev.Next = next;
+            }
+
+            Count--;
+            GC.Collect();
+        }
+
+        /// <summary>
+        /// Метод для получения элемента списка по индексу.
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        public T GetElement(int index)
+        {
+            if ((Count - index) > index)
+            {
+                Node<T> current = Head;
+
+                for (int i = 0; current != null; i++)
+                {
+                    if (i == index)
+                    {
+                        return current.Data;
+                    }
+                    current = current.Next;
+                }
+            }
+            else
+            {
+                Node<T> current = Tail;
+
+                for (int i = Count -1; current != null; i--)
+                {
+                    if (i == index)
+                    {
+                        return current.Data;
+                    }
+                    current = current.Prev;
+                }
+            }         
+
+            return default(T);
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            var current = Head;
+
+            while (current != null)
+            {
+                yield return current;
+                current = current.Next;
+            }
+        }
+
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            return (IEnumerator<T>)GetEnumerator();
+        }
+    }
+}
---
+++ TestTask/Program.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DoublyLinkedList<string> list = new DoublyLinkedList<string>();
+            int userChoice = default(int);
+            string inpStr = string.Empty;
+            int index = default(int);
+
+            while (true)
+            {
+                Console.WriteLine("**************************************************");
+                Console.WriteLine("\t\tМеню пользователя\n");
+
+                Console.WriteLine("{0} - добавить элемент в конец списка;", (int)Menu.Add);
+                Console.WriteLine("{0} - добавить элемент в список по индексу;", (int)Menu.Insert);
+                Console.WriteLine("{0} - удалить элемент из списка по индексу;", (int)Menu.RemoveAt);
+                Console.WriteLine("{0} - получить элемент списка по индексу;", (int)Menu.GetElement);
+                Console.WriteLine("{0} - получить количество элементов в списке;", (int)Menu.GetCount);
+                Console.WriteLine("{0} - показать весь список;", (int)Menu.ShowList);
+                Console.WriteLine("{0} - выход.\n", (int)Menu.Exit);
+
+                Console.Write("Введите номер опции: ");
+                try
+                {
+                    userChoice = int.Parse(Console.ReadLine());
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    continue;
+                }
+
+                switch (userChoice)
+                {
+                    case (int)Menu.Exit:
+                        return;
+
+                    case (int)Menu.Add:
+                        Console.Write("Введите строку: ");
+                        inpStr = Console.ReadLine();
+                        list.Add(inpStr);
+                        Console.WriteLine("Операция выполнена.");
+                        break;
+
+                    case (int)Menu.Insert:
+                        Console.Write("Введите строку: ");
+                        inpStr = Console.ReadLine();
+                        index = GetIndexFromUser();
+
+                        try
+                        {
+                            if (index < 0 || index > list.Count)
+                            {
+                                throw new IndexOutOfRangeException("Индекс вышел за пределы диапазона.");
+                            }
+                            list.Insert(inpStr, index);
+                            Console.WriteLine("Операция выполнена.");
+                        }
+                        catch (IndexOutOfRangeException ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+
+                        break;
+
+                    case (int)Menu.RemoveAt:
+                        index = GetIndexFromUser();
+
+                        try
+                        {
+                            if (index < 0 || index > list.Count - 1 || list.Count == 0)
+                            {
+                                throw new IndexOutOfRangeException("Индекс вышел за пределы диапазона.");
+                            }
+                            list.RemoveAt(index);
+                            Console.WriteLine("Операция выполнена.");
+                        }
+                        catch (IndexOutOfRangeException ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+
+                        break;
+
+                    case (int)Menu.GetElement:
+                        index = GetIndexFromUser();
+
+                        try
+                        {
+                            if (index < 0 || index > list.Count - 1 || list.Count == 0)
+                            {
+                                throw new IndexOutOfRangeException("Индекс вышел за пределы диапазона.");
+                            }
+                            Console.WriteLine(list.GetElement(index));
+                        }
+                        catch (IndexOutOfRangeException ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+
+                        break;
+
+                    case (int)Menu.GetCount:
+                        Console.WriteLine($"Количество элементов в списке: {list.Count}");
+                        break;
+
+                    case (int)Menu.ShowList:
+                        foreach (var item in list)
+                        {
+                            Console.WriteLine(item);
+                        }
+                        break;
+                }
+            }
+        }
+
+        enum Menu
+        {
+            Exit, Add, Insert, RemoveAt, GetElement, GetCount, ShowList
+        };
+
+        static int GetIndexFromUser()
+        {
+            int index = default(int);
+
+            while (true)
+            {
+                Console.Write("Введите индекс: ");
+
+                try
+                {
+                    index = int.Parse(Console.ReadLine());
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    continue;
+                }
+
+                break;
+            }
+
+            return index;
+        }
+    }
+}
